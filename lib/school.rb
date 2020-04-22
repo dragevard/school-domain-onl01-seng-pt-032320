489719e1c79ad488ee2b97130541de07f677ea56
Derek phung <dragevards@gmail.com>
2020-04-22 20:18:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   
   
   def add_student(name, grade)
-    if @roster[grade].include?(grade)
+    if @roster.include?(grade)
       @roster[grade] << name
     else 
       @roster[grade] = []
